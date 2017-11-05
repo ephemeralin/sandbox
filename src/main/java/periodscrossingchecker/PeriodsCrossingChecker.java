45a982c3cfa3d7217cd27e52be6c3893ab8f91a0
ephemeralin <ephemeralin@gmail.com>
2017-11-05 22:41:58 +0300

    Period crossing checker done. Refactor code to fix problems

--- src/main/java/periodscrossingchecker/PeriodsCrossingChecker.java
+++ src/main/java/periodscrossingchecker/PeriodsCrossingChecker.java
@@ -1,77 +1,86 @@
 package periodscrossingchecker;
 
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 /**
  * The Periods crossing checker.
  */
 public class PeriodsCrossingChecker {
-
-    /**
-     * The constant BEGINNING_OF_CALENDAR.
-     */
-    public static final GregorianCalendar BEGINNING_OF_CALENDAR = new GregorianCalendar();
-    /**
-     * The constant END_OF_CALENDAR.
-     */
-    public static final GregorianCalendar END_OF_CALENDAR = new GregorianCalendar();
-
-    static {
-        BEGINNING_OF_CALENDAR.set(Calendar.MILLISECOND, 0);
-        END_OF_CALENDAR.setTimeInMillis(Long.MAX_VALUE);
-    }
-
     /**
      * Main method for to check crossing periods.
      *
-     * @param firstStartDate  the first start date
-     * @param firstEndDate    the first end date
-     * @param secondStartDate the second start date
-     * @param secondEndDate   the second end date
+     * @param firstDatesInterval  the first period
+     * @param secondDatesInterval the second period
      * @return the int
      */
-    public int check(GregorianCalendar firstStartDate, GregorianCalendar firstEndDate,
-                               GregorianCalendar secondStartDate, GregorianCalendar secondEndDate) {
-        Period firstPeriod = new Period(firstStartDate, firstEndDate);
-        Period secondPeriod = new Period(secondStartDate, secondEndDate);
+    public DatesInterval check(DatesInterval firstDatesInterval, DatesInterval secondDatesInterval) {
+        DatesInterval crossingDatesInterval;
 
-        return check(firstPeriod, secondPeriod);
-    }
+        LocalDate startDateFirst = firstDatesInterval.getStartDate();
+        LocalDate endDateFirst = firstDatesInterval.getEndDate();
+        LocalDate startDateSecond = secondDatesInterval.getStartDate();
+        LocalDate endDateSecond = secondDatesInterval.getEndDate();
 
-    /**
-     * Main method for to check crossing periods.
-     *
-     * @param firstPeriod  the first period
-     * @param secondPeriod the second period
-     * @return the int
-     */
-    public int check(Period firstPeriod, Period secondPeriod) {
-        if (secondPeriod.getStartDate().compareTo(firstPeriod.getEndDate()) < 0) {
-            //crossing
-            return -1;
-        } else if (secondPeriod.getStartDate().compareTo(firstPeriod.getEndDate()) > 0) {
-            //no crossing
-            return 1;
+        if (startDateSecond.compareTo(startDateFirst) < 0 & endDateSecond.compareTo(startDateFirst) < 0) {        //1
+            crossingDatesInterval = new DatesInterval();
+
+        } else if (startDateSecond.compareTo(startDateFirst) < 0 & endDateSecond.compareTo(startDateFirst) >= 0
+                & endDateSecond.compareTo(endDateFirst) < 0) { //2
+            crossingDatesInterval = new DatesInterval(startDateFirst, endDateSecond);
+
+        } else if (startDateSecond.compareTo(startDateFirst) >= 0 & endDateSecond.compareTo(endDateFirst) <= 0) { //3
+            crossingDatesInterval = new DatesInterval(startDateSecond, endDateSecond);
+
+        } else if (startDateSecond.compareTo(endDateFirst) <= 0 & endDateSecond.compareTo(endDateFirst) > 0
+                & startDateFirst.compareTo(startDateFirst) > 0) { //4
+            crossingDatesInterval = new DatesInterval(startDateSecond, endDateFirst);
+
+        } else if (startDateSecond.compareTo(endDateFirst) > 0 & endDateSecond.compareTo(endDateFirst) > 0) { //5
+            crossingDatesInterval = new DatesInterval();
+
+        } else if (startDateSecond.compareTo(startDateFirst) < 0 & endDateSecond.compareTo(endDateFirst) > 0) { //6
+            crossingDatesInterval = new DatesInterval(startDateFirst, endDateFirst);
+
+        } else if (startDateSecond.compareTo(endDateFirst) < 0) {
+            crossingDatesInterval = new DatesInterval(startDateSecond, endDateFirst);
+
+        } else if (endDateSecond.compareTo(startDateFirst) < 0) {
+            crossingDatesInterval = new DatesInterval(startDateFirst, endDateSecond);
+
         } else {
-            //crossing in one date
-            return 0;
+            crossingDatesInterval = new DatesInterval();
         }
+
+        return crossingDatesInterval;
     }
 
     /**
      * The type Period.
      */
-    static class Period {
+    static class DatesInterval {
         /**
          * Start date of the period.
          */
-        private final GregorianCalendar startDate;
+        private final LocalDate startDate;
         /**
          * * End date of the period.
          */
-        private final GregorianCalendar endDate;
+        private final LocalDate endDate;
+
+        /**
+         * Dates interval is empty.
+         */
+        private boolean isEmpty;
+
+        /**
+         * Instantiates a new empty Period.
+         */
+        DatesInterval() {
+            this.startDate = null;
+            this.endDate = null;
+            this.isEmpty = true;
+        }
 
         /**
          * Instantiates a new Period.
@@ -79,9 +88,18 @@ public class PeriodsCrossingChecker {
          * @param startDate the start date
          * @param endDate   the end date
          */
-        Period(GregorianCalendar startDate, GregorianCalendar endDate) {
-            this.startDate = startDate;
-            this.endDate = endDate;
+        DatesInterval(LocalDate startDate, LocalDate endDate) {
+            if (startDate == null) {
+                this.startDate = LocalDate.MIN;
+            } else {
+               this.startDate = startDate;
+            }
+            if (endDate == null) {
+                this.endDate = LocalDate.MAX;
+            } else {
+                this.endDate = endDate;
+            }
+            this.isEmpty = false;
         }
 
         /**
@@ -89,7 +107,7 @@ public class PeriodsCrossingChecker {
          *
          * @return the start date
          */
-        public GregorianCalendar getStartDate() {
+        public LocalDate getStartDate() {
             return startDate;
         }
 
@@ -98,9 +116,36 @@ public class PeriodsCrossingChecker {
          *
          * @return the end date
          */
-        public GregorianCalendar getEndDate() {
+        public LocalDate getEndDate() {
             return endDate;
         }
-    }
 
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            DatesInterval that = (DatesInterval) o;
+
+            if (isEmpty != that.isEmpty) {
+                return false;
+            }
+            if (getStartDate() != null ? !getStartDate().equals(that.getStartDate()) : that.getStartDate() != null) {
+                return false;
+            }
+            return getEndDate() != null ? getEndDate().equals(that.getEndDate()) : that.getEndDate() == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = getStartDate() != null ? getStartDate().hashCode() : 0;
+            result = 31 * result + (getEndDate() != null ? getEndDate().hashCode() : 0);
+            result = 31 * result + (isEmpty ? 1 : 0);
+            return result;
+        }
+    }
 }
--- src/test/java/periodscrossingchecker/PeriodsCrossingCheckerTest.java
+++ src/test/java/periodscrossingchecker/PeriodsCrossingCheckerTest.java
@@ -2,7 +2,7 @@ package periodscrossingchecker;
 
 import org.junit.Test;
 
-import java.util.GregorianCalendar;
+import java.time.LocalDate;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -18,17 +18,116 @@ public class PeriodsCrossingCheckerTest {
      * @throws Exception the exception
      */
     @Test
-    public void whenPeriodsAreCrossedThenHasParticularCrossingPeriodTest() throws Exception {
-        PeriodsCrossingChecker.Period firstPeriod =
-                new PeriodsCrossingChecker.Period(new GregorianCalendar(2000, 0, 1),
-                        new GregorianCalendar(2010, 1, 1));
-        PeriodsCrossingChecker.Period secondPeriod =
-                new PeriodsCrossingChecker.Period(new GregorianCalendar(2008, 3, 10),
-                        new GregorianCalendar(2015, 2, 8));
+    public void normalCrossingTest() throws Exception {
+        PeriodsCrossingChecker.DatesInterval firstDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2000, 1, 1),
+                        LocalDate.of(2010, 1, 1));
+        PeriodsCrossingChecker.DatesInterval secondDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2008, 3, 10),
+                        LocalDate.of(2015, 2, 8));
 
         PeriodsCrossingChecker pcc = new PeriodsCrossingChecker();
+        PeriodsCrossingChecker.DatesInterval resultDatesInterval = pcc.check(firstDatesInterval, secondDatesInterval);
 
-        assertThat(pcc.check(firstPeriod, secondPeriod), is(-1));
+        PeriodsCrossingChecker.DatesInterval expectedDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2008, 3, 10),
+                        LocalDate.of(2010, 1, 1));
+
+        assertThat(resultDatesInterval.equals(expectedDatesInterval), is(true));
     }
 
+    /**
+     * Second period is minimal start date test.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void secondPeriodIsMinimalStartDateTest() throws Exception {
+        PeriodsCrossingChecker.DatesInterval firstDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2000, 1, 1),
+                        LocalDate.of(2010, 1, 1));
+        PeriodsCrossingChecker.DatesInterval secondDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(null,
+                        LocalDate.of(2015, 1, 1));
+
+        PeriodsCrossingChecker pcc = new PeriodsCrossingChecker();
+        PeriodsCrossingChecker.DatesInterval resultDatesInterval = pcc.check(firstDatesInterval, secondDatesInterval);
+
+        PeriodsCrossingChecker.DatesInterval expectedDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2000, 1, 1),
+                        LocalDate.of(2010, 1, 1));
+
+        assertThat(resultDatesInterval.equals(expectedDatesInterval), is(true));
+    }
+
+    /**
+     * First has max end date and second inside test.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void firstHasMaxEndDateAndSecondInsideTest() throws Exception {
+        PeriodsCrossingChecker.DatesInterval firstDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2000, 1, 1),
+                        null);
+        PeriodsCrossingChecker.DatesInterval secondDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2008, 1, 1),
+                        LocalDate.of(2015, 1, 1));
+
+        PeriodsCrossingChecker pcc = new PeriodsCrossingChecker();
+        PeriodsCrossingChecker.DatesInterval resultDatesInterval = pcc.check(firstDatesInterval, secondDatesInterval);
+
+        PeriodsCrossingChecker.DatesInterval expectedDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2008, 1, 1),
+                        LocalDate.of(2015, 1, 1));
+
+        assertThat(resultDatesInterval.equals(expectedDatesInterval), is(true));
+    }
+
+    /**
+     * First has max end date and second cross test.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void firstHasMaxEndDateAndSecondCrossTest() throws Exception {
+        PeriodsCrossingChecker.DatesInterval firstDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2000, 1, 1),
+                        null);
+        PeriodsCrossingChecker.DatesInterval secondDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(1990, 1, 1),
+                        LocalDate.of(2015, 1, 1));
+
+        PeriodsCrossingChecker pcc = new PeriodsCrossingChecker();
+        PeriodsCrossingChecker.DatesInterval resultDatesInterval = pcc.check(firstDatesInterval, secondDatesInterval);
+
+        PeriodsCrossingChecker.DatesInterval expectedDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2000, 1, 1),
+                        LocalDate.of(2015, 1, 1));
+
+        assertThat(resultDatesInterval.equals(expectedDatesInterval), is(true));
+    }
+
+    /**
+     * Intervals do not crosses test.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void intervalsDoNotCrossesTest() throws Exception {
+        PeriodsCrossingChecker.DatesInterval firstDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2000, 1, 1),
+                        LocalDate.of(2008, 1, 1));
+        PeriodsCrossingChecker.DatesInterval secondDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval(LocalDate.of(2010, 1, 1),
+                        LocalDate.of(2015, 1, 1));
+
+        PeriodsCrossingChecker pcc = new PeriodsCrossingChecker();
+        PeriodsCrossingChecker.DatesInterval resultDatesInterval = pcc.check(firstDatesInterval, secondDatesInterval);
+
+        PeriodsCrossingChecker.DatesInterval expectedDatesInterval =
+                new PeriodsCrossingChecker.DatesInterval();
+
+        assertThat(resultDatesInterval.equals(expectedDatesInterval), is(true));
+    }
 }
